c433fbeccb4ed0576af57e3193861d91f22d7ff6
SlavikusVOG <slavaolish@gmail.com>
2025-11-30 02:44:05 +0300

    fix: fix app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './entities/user/user.service';
-import { ArtistService } from './entities/artist/artist.service';
-import { TrackService } from './entities/track/track.service';
-import { AlbumService } from './entities/album/album.service';
-import { FavoritesService } from './entities/favorites/favorites.service';
-import { UserController } from './entities/user/user.controller';
-import { ArtistController } from './entities/artist/artist.controller';
-import { TrackController } from './entities/track/track.controller';
-import { AlbumController } from './entities/album/album.controller';
-import { FavoritesController } from './entities/favorites/favorites.controller';
-import { UserModule } from './entities/user/user.module';
-import { ArtistModule } from './entities/artist/artist.module';
-import { TrackModule } from './entities/track/track.module';
-import { AlbumModule } from './entities/album/album.module';
-import { FavoritesModule } from './entities/favorites/favorites.module';
+
 import { UsersModule } from './users/users.module';
 import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
@@ -23,8 +9,24 @@ import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [UserModule, ArtistModule, TrackModule, AlbumModule, FavoritesModule, UsersModule, ArtistsModule, TracksModule, AlbumsModule],
-  controllers: [AppController, UserController, ArtistController, TrackController, AlbumController, FavoritesController],
-  providers: [AppService, UserService, ArtistService, TrackService, AlbumService, FavoritesService],
+  imports: [
+    UsersModule,
+    ArtistsModule,
+    TracksModule,
+    AlbumsModule,
+    FavoritesModule,
+    UsersModule,
+    ArtistsModule,
+    TracksModule,
+    AlbumsModule,
+  ],
+  controllers: [
+    AppController,
+    AbortController,
+    AppController,
+    AppController,
+    AbortController,
+  ],
+  providers: [AppService],
 })
 export class AppModule {}
