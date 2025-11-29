88c6c2ad7cdd1f8c7923d0f65c12e36e5affdbee
SlavikusVOG <slavaolish@gmail.com>
2025-11-30 02:08:32 +0300

    feat: add controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,15 @@ import { ArtistService } from './entities/artist/artist.service';
 import { TrackService } from './entities/track/track.service';
 import { AlbumService } from './entities/album/album.service';
 import { FavoritesService } from './entities/favorites/favorites.service';
+import { UserController } from './entities/user/user.controller';
+import { ArtistController } from './entities/artist/artist.controller';
+import { TrackController } from './entities/track/track.controller';
+import { AlbumController } from './entities/album/album.controller';
+import { FavoritesController } from './entities/favorites/favorites.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UserController, ArtistController, TrackController, AlbumController, FavoritesController],
   providers: [AppService, UserService, ArtistService, TrackService, AlbumService, FavoritesService],
 })
 export class AppModule {}
