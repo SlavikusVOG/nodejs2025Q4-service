6ffc6c1abbe68248211df38ec89b3c7a73954977
SlavikusVOG <slavaolish@gmail.com>
2025-11-30 02:06:23 +0300

    feat: add services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './entities/user/user.service';
+import { ArtistService } from './entities/artist/artist.service';
+import { TrackService } from './entities/track/track.service';
+import { AlbumService } from './entities/album/album.service';
+import { FavoritesService } from './entities/favorites/favorites.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, ArtistService, TrackService, AlbumService, FavoritesService],
 })
 export class AppModule {}
