ea51e7e11dbb0d86a637dd5bc2ac49f4403d0816
SlavikusVOG <slavaolish@gmail.com>
2025-12-04 03:24:37 +0300

    feat: move swagger schema generator

--- src/generate-swagger.ts
+++ src/generate-swagger.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { writeFileSync } from 'fs';
-import { AppModule } from './src/app.module';
+import { AppModule } from './app.module';
 import * as yaml from 'js-yaml';
 
 async function generateSwaggerSpec() {
