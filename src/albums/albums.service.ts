3f50d2d2c6f9092929ca3909c2564c5570182f87
SlavikusVOG <slavaolish@gmail.com>
2025-12-20 07:33:35 +0300

    chore: remove comment

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
-// import { Database } from '../database/db/db.interface';
 import { TypeOrmDatabaseService } from '../database/typeorm/typeorm-database.service';
 
 @Injectable()
